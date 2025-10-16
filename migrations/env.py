7d46367ecde349d7f7d5a013a91a4c9bfec21e70
jvras58 <jvras@cin.ufpe.br>
2025-10-16 09:52:00 -0300

    refactor: ajustar formatação de strings e remover arquivo de migração obsoleto

--- migrations/env.py
+++ migrations/env.py
@@ -37,7 +37,7 @@ for module in app_models:
     try:
         loaded_module = importlib.import_module(module)
     except ModuleNotFoundError:
-        print(f'Could not import module {module}') # TODO: log this
+        print(f'Could not import module {module}')   # TODO: log this
 
 target_metadata = Base.metadata
 # --------------------------
@@ -60,12 +60,12 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option('sqlalchemy.url')
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+        dialect_opts={'paramstyle': 'named'},
     )
 
     with context.begin_transaction():
@@ -81,7 +81,7 @@ def run_migrations_online() -> None:
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+        prefix='sqlalchemy.',
         poolclass=pool.NullPool,
     )
 
