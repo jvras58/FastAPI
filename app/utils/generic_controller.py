755ec0b72cb0f0d34934baaa53726297e06b2bd0
jvras58 <jvras@cin.ufpe.br>
2025-10-16 09:53:58 -0300

    refactor: melhorar a tipagem e a legibilidade do GenericController

--- app/utils/generic_controller.py
+++ app/utils/generic_controller.py
@@ -1,25 +1,37 @@
+"""Generic controller for CRUD operations."""
+from typing import Generic, TypeVar
+
 from sqlalchemy import String, and_, select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from app.utils.base_model import AbstractBaseModel
-from app.utils.exceptions import IntegrityValidationException, ObjectNotFoundException
+from app.utils.exceptions import (
+    IntegrityValidationException,
+    ObjectNotFoundException,
+)
+
+T = TypeVar('T', bound=AbstractBaseModel)
 
 
-class GenericController:
-    def __init__(self, model: AbstractBaseModel) -> None:
-        self.model: AbstractBaseModel = model
+class GenericController(Generic[T]):
+    """Generic controller for CRUD operations."""
 
-    def get(self, db_session: Session, obj_id: int) -> AbstractBaseModel:
+    def __init__(self, model: type[T]) -> None:
+        """Initialize the controller with a specific model."""
+        self.model: type[T] = model
 
+    def get(self, db_session: Session, obj_id: int) -> T:
+        """Get an object by its ID."""
         instance = db_session.get(self.model, obj_id)
         if not instance:
-            raise ObjectNotFoundException(self.model.__name__, obj_id)
+            raise ObjectNotFoundException(self.model.__name__, str(obj_id))
         return instance
 
     def get_all(
         self, db_session: Session, skip: int = 0, limit: int = 100, **kwargs
-    ) -> list[AbstractBaseModel]:
+    ) -> list[T]:
+        """Get all objects with optional filtering and pagination."""
         query = select(self.model)
         if kwargs:
             criteria_and = []
@@ -32,17 +44,19 @@ class GenericController:
 
             query = query.filter(and_(*criteria_and))
 
-        return db_session.scalars(query.offset(skip).limit(limit)).all()
+        return list(db_session.scalars(query.offset(skip).limit(limit)).all())
 
-    def delete(self, db_session: Session, id: int) -> None:
-        instance = self.get(db_session, id)
+    def delete(self, db_session: Session, obj_id: int) -> None:
+        """Delete an object by its ID."""
+        instance = self.get(db_session, obj_id)
         if not instance:
-            raise ObjectNotFoundException(self.model.__name__, id)
+            raise ObjectNotFoundException(self.model.__name__, str(obj_id))
 
         db_session.delete(instance)
         db_session.commit()
 
-    def save(self, db_session: Session, obj: AbstractBaseModel) -> AbstractBaseModel:
+    def save(self, db_session: Session, obj: T) -> T:
+        """Save a new object to the database."""
         try:
             db_session.add(obj)
             db_session.commit()
@@ -52,10 +66,17 @@ class GenericController:
             raise IntegrityValidationException(exc.args[0]) from exc
         return obj
 
-    def update(self, db_session: Session, obj: AbstractBaseModel) -> AbstractBaseModel:
-        instance = self.get(db_session, obj.id)
+    def update(self, db_session: Session, obj: T) -> T:
+        """Update an existing object in the database."""
+        obj_id = getattr(obj, 'id', None)
+        if obj_id is None:
+            raise ValueError(
+                "Object must have an 'id' attribute for update operations"
+            )
+
+        instance = self.get(db_session, obj_id)
         if not instance:
-            raise ObjectNotFoundException(self.model.__name__, obj.id)
+            raise ObjectNotFoundException(self.model.__name__, str(obj_id))
 
         for key, value in obj.as_dict().items():
             setattr(instance, key, value)
