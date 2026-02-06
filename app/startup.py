b086a366b3f1a2014a076eefa995dc3e0110c7ac
jvras58 <jvras@cin.ufpe.br>
2026-02-05 21:37:52 -0300

    refactor: remover importação e inclusão do roteador de processamento de texto

--- app/startup.py
+++ app/startup.py
@@ -7,7 +7,6 @@ from app.api.authentication.router import router as auth_router
 from app.api.authorization.middleware import AuthorizationMiddleware
 from app.api.authorization.router import router as authorization_router
 from app.api.role.router import router as role_router
-from app.api.text_processing.router import router as text_processing_router
 from app.api.transaction.router import router as transaction_router
 from app.api.user.router import router as user_router
 
@@ -83,10 +82,7 @@ app.include_router(
     assignment_router, prefix='/assignment', tags=['Assignments']
 )
 app.include_router(
-    authorization_router, prefix='/authorization', tags=['Authorizations']
-)
-app.include_router(
-    text_processing_router, prefix='/text-processing', tags=['Text Processing']
+    authorization_router, prefix="/authorization", tags=["Authorizations"]
 )
 # ----------------------------------
 
